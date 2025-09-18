d8bb84cd272393d6ede46306cb48d1984705c2ca
YNMDR <muralidharreddy20bcs25@iiitkottayam.ac.in>
2025-09-18 14:57:16 +0530

    added files 2

--- backend/create_conn.py
+++ backend/create_conn.py
@@ -7,6 +7,6 @@ def connect_db():
   print("connecting to database:")
   global __con
   if __con is None:
-    __con = mysql.connector.connect(user='root', password='Doremon@77', database='g_dbase')
+    __con = mysql.connector.connect(user='username', password='Password', database='databasename')
 
   return __con
